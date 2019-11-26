b24f5bd5e399f52941f351cc9ce4a160f17609de
bloldf <48572660+bloldf@users.noreply.github.com>
2019-11-26 15:31:11 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  TOKEN: "NjQ0OTczNTczMzMyNTMzMjc2.XdAq0w.eXgGqLvemGh_Yx2hObtxvI8owfs",
+  YT_API_KEY: "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4",
+  prefix: "$",
+  devs: ["532325977921945603"]
+};
